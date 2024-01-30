4ef66c87f26973cef6f46d243682f5048574c882
Danial Amin <danialamin489@gmail.com>
2024-01-31 02:51:50 +0500

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <div>
-      <p className='text-white'>hufhf</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
